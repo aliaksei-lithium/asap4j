51af9164c8d7c37b75dd1d39f2498b32bd3f9c77
Maksim Golunko <maksim_golunko@epam.com>
2013-12-07 18:29:42 +0300

    feature save

--- src/main/java/com/epam/asap4j/controller/FeatureController.java
+++ src/main/java/com/epam/asap4j/controller/FeatureController.java
@@ -1,5 +1,10 @@
 package com.epam.asap4j.controller;
 
+import com.epam.asap4j.dao.EventDao;
+import com.epam.asap4j.dao.FeatureDao;
+import com.epam.asap4j.dto.Event;
+import com.epam.asap4j.dto.Feature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +13,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/features")
 public class FeatureController {
 
+    @Autowired
+    private EventDao eventDao;
+
     @RequestMapping(method = RequestMethod.GET)
     public String doIt() {
         return "layout";
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public String saveFeature(Event event) {
+        eventDao.saveOrUpdate(event);
+        return "redirect:dashboard";
+    }
 }
