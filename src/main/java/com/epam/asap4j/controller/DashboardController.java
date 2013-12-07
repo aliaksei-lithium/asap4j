69f9d6989b668d797a9b3895ee8e5bce65700740
Andrei_Akatsyeu <Andrei_Akatsyeu@epam.com>
2013-12-07 18:38:50 +0300

    add controller methods

--- src/main/java/com/epam/asap4j/controller/DashboardController.java
+++ src/main/java/com/epam/asap4j/controller/DashboardController.java
@@ -1,20 +1,52 @@
 package com.epam.asap4j.controller;
 
+import com.epam.asap4j.dao.EventDao;
+import com.epam.asap4j.dao.FeatureDao;
+import com.epam.asap4j.dao.UserDao;
+import com.epam.asap4j.dto.Feature;
+import com.epam.asap4j.dto.Person;
+import com.epam.asap4j.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class DashboardController {
 
+    @Autowired
+    private EventDao eventDao;
+
+    @Autowired
+    private FeatureDao featureDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    private static final Person DEFAULT_USER = new Person("4060741400006438769","Aliaksei Zhynhiarouski");
+
     @RequestMapping(method = RequestMethod.GET)
-    public String index() {
+    public String index(HttpSession session, @RequestParam(value = "userName") String userName) {
+        if(session.getAttribute("userBean")==null){
+            session.setAttribute("userBean", userDao.getByName(userName != null ? userName : DEFAULT_USER.getPersonName()));
+        }
         return "redirect:dashboard";
     }
 
     @RequestMapping(value = "dashboard", method = RequestMethod.GET)
-    public String getDashboard() {
+    public String getDashboard(HttpSession session, ModelMap model) {
+        List<Feature> features = featureDao.getEntitiesList();
+        for(Feature feature : features){
+            User user = (User)session.getAttribute("userBean");
+            model.addAttribute(feature.getFeatureName(), eventDao.getPersonEventsByFeature(user != null ? user.getPerson() : DEFAULT_USER, feature));
+        }
+
         return "dashboard/dashboard-main";
     }
 
