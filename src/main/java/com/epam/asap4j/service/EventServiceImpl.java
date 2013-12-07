c6db2a45302214cab4a070aacd726bc862f13031
Andrei_Akatsyeu <Andrei_Akatsyeu@epam.com>
2013-12-07 20:00:45 +0300

    Fix

--- src/main/java/com/epam/asap4j/service/EventServiceImpl.java
+++ src/main/java/com/epam/asap4j/service/EventServiceImpl.java
@@ -45,7 +45,9 @@ public class EventServiceImpl implements EventService {
         for(Person person : personDao.getEntitiesList()){
             if(eventDao.hasEventByExternalId(person.getPersonId(), BIRTH_DAY)){
                 Event event = new Event(person.getPersonName(), BIRTH_DAY, groupService.getPersonGroups(person));
-                event.setDate(employeeInfoDao.getEntityById(person.getPersonId()).getBirthday());
+                Date date = employeeInfoDao.getEntityById(person.getPersonId()).getBirthday();
+                date.setYear(2013);
+                event.setDate(date);
                 eventDao.saveOrUpdate(event);
             }
         }
